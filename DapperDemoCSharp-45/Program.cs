346b94c1b69606e2077bf693c33991987cde6e5e
Seth Bowman <brandonsethbowman@gmail.com>
2024-03-19 10:58:18 -0500

    demo completed

---
+++ DapperDemoCSharp-45/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperDemoCSharp_45;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DepartmentRepo(conn);
+
+var departments = departmentRepo.GetAllDepartments();
+
+departmentRepo.InsertDepartment("Cooler Stuff");
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.Name);
+}
